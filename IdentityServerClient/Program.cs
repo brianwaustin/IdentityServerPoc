74fffd6484dc1b4a75872ebdee3d62f625f43c14
baustin <baustin@emporos.net>
2018-10-23 10:44:15 -0400

    Initial add of the proof of concept code

---
+++ IdentityServerClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServerClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
+
---
+++ IdentityServerPoc/Startup.cs
@@ -0,0 +1,70 @@
+﻿using IdentityServer4.AccessTokenValidation;
+using IdentityServer4.Validation;
+//using IdentityServerPoc.Infrastructure.Security;
+//using IdentityServerPoc.Infrastructure.Security.Entities;
+//using IdentityServerPoc.Infrastructure.Security.Interfaces;
+//using IdentityServerPoc.Infrastructure.Security.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace IdentityServerPoc
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //services.AddSingleton<IPostConfigureOptions<CustomAuthenticationOptions>, ApplicationKeyAuthenticationPostConfigureOptions>();
+            //services.AddTransient<IExtensionGrantValidator, ApplicationKeyGrantValidator>();
+
+            /* Configure identity server with in-memory stores, keys, clients and scopes */
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
+
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();            
+
+            services.AddAuthentication("Bearer")
+             .AddIdentityServerAuthentication(options =>
+             {
+                 options.Authority = "http://localhost:5001";
+                 options.RequireHttpsMetadata = false;
+
+                 options.ApiName = "api1";
+             });
+
+            /* Custom authentication header */
+            //services.AddAuthentication(CustomAuthenticationDefaults.AuthenticationSchemeAppKey)
+            //    .AddAppKey<ApplicationKeyAuthenticationService>(o =>
+            //    {
+            //        o.Realm = "api1";
+            //    });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            /* Registers the IdentityServer services in DI. It also registers an in-memory store for runtime state. 
+               Useful to get started, but needs to be replaced by some persistent key material for production scenarios. */
+            app.UseIdentityServer();
+            app.UseAuthentication();
+            app.UseMvc();
+            
+        }
+    }
+}
